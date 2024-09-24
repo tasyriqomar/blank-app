9835647c1bbe3389bcaab02074521de441708c00
tasyriqomar <121738766+tasyriqomar@users.noreply.github.com>
2024-09-24 13:48:45 +0000

    Update content

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 
-st.title("🎈 My new Streamlite app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.write("Hello World")
+x = st.text_input("Favourite Movies?")
+st.write(f"Your favourite movie is: {x}")
